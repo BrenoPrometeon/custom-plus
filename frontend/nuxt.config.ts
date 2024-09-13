1eca1ce254f2fbde319a2e2721194c6dae4312ef
BrenoPrometeon <breno.soares.st@prometeon.com>
2024-09-12 21:43:10 -0300

    :sparkles: feat: useQuery and UseFetch

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,12 +6,27 @@ export default defineNuxtConfig({
       pageTransition: { name: 'page', mode: 'out-in' },
   },
 
-  modules: ['d-naive',"@nuxt/icon"],
+  modules: ['d-naive',"@nuxt/icon", "@nuxtjs/apollo"],
   ssr: false,
   css: ['vuetify/lib/styles/main.sass'],
 
   build: {
       transpile: ['vuetify'],
+  },
+
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: process.env.API_URL + "graphql",
+        tokenStorage: "cookie",
+        httpLinkOptions: {
+          credentials: "include",
+        },
+        inMemoryCacheOptions: {
+          addTypename: false,
+        },
+      },
+    },
   },
 
   runtimeConfig: {
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div class="text-h1"><v-card class="bg-primary">PAULO VIADO</v-card></div>
+  <div><v-card class="bg-primary">PAULO VIADO</v-card>{{ customers }}{{ result }}</div>
 </template>
+<script setup>
+const query = gql`
+  query MyQuery {
+    customers {
+      address
+      email
+      id
+      name
+      phone
+    }
+  }
+`;
+
+const { result } = useQuery(query)
+const { data: customers} = useApi('/customer/')
+console.log(result.value)
+</script>
