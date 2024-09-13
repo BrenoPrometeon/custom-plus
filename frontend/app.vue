0aae59189bfa3168d43b82bd004951c11ad636cf
BrenoPrometeon <breno.soares.st@prometeon.com>
2024-09-13 19:21:00 -0300

    :construction: Front

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,12 +1,32 @@
 <template>
   <v-app class="bg-grey-lighten-2 overflow-auto">
-    <v-container class="pt-0">
-      <v-row justify="center" no-gutters>
-        <v-col cols="10"><NuxtPage /></v-col> </v-row
-    ></v-container>
+    <v-app-bar color="primary"> </v-app-bar
+    ><v-card>
+      <div class="d-flex flex-row">
+        <v-tabs direction="vertical">
+          <v-tab
+            v-for="item in items"
+            :to="item.value"
+            :text="item.title"
+          ></v-tab
+        ></v-tabs>
+        <v-tabs-window>
+          <v-tabs-window-item>
+            <v-card flat><NuxtPage /></v-card></v-tabs-window-item
+        ></v-tabs-window></div
+    ></v-card>
   </v-app>
 </template>
 
 <script setup>
-
+const items = [
+  {
+    title: "Dashboard",
+    value: "/",
+  },
+  {
+    title: "Indicadores",
+    value: "products",
+  },
+];
 </script>
