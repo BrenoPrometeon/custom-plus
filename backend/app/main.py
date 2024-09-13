b2bce774b425355fb342eea536dfc0f8329f1d7b
BrenoPrometeon <breno.soares.st@prometeon.com>
2024-09-12 21:07:57 -0300

    :sparkles: feat: Backend Class

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
 import os
+from typing import List
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Session, SQLModel, create_engine, select
 from graphemy import Graphemy, GraphemyRouter, import_files
-from .models import *
+from .models.customer import Customer
+from .models.order import Order
+from .models.product import Product
+from .models.purchases import Purchases
+from .models.stock import Stock
+from .models.supplier import Supplier
 
 from .dependencies import engine, settings
 
@@ -27,9 +34,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-## READ
+@app.get("/customer/")
+async def read_customer():
+    with Session(engine) as session:
+        indicator = session.exec(select(Customer)).all()
+        return indicator
 
-@app.get("/")
-async def read_indicator():
-    print('Hello World')
-    return None
+@app.get("/order/")
+async def read_order():
+    with Session(engine) as session:
+        indicator = session.exec(select(Order)).all()
+        return indicator
+
+@app.get("/product/")
+async def read_product():
+    with Session(engine) as session:
+        indicator = session.exec(select(Product)).all()
+        return indicator
+
+@app.get("/purchase/")
+async def read_purchase():
+    with Session(engine) as session:
+        indicator = session.exec(select(Purchases)).all()
+        return indicator
+
+@app.get("/stock/")
+async def read_stock():
+    with Session(engine) as session:
+        indicator = session.exec(select(Stock)).all()
+        return indicator
+
+@app.get("/supplier/")
+async def read_supplier():
+    with Session(engine) as session:
+        indicator = session.exec(select(Supplier)).all()
+        return indicator
--- backend/app/models/customer.py
+++ backend/app/models/customer.py
@@ -1,6 +1,5 @@
 from graphemy import Field, Graphemy, Dl
 
-
 class Customer(Graphemy, table=True):
     id: int | None = Field(primary_key=True, default=None)
     name: str
